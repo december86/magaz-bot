fbf21aab62e5d1534daa33c7f0a8d50c10a9fd69
krivoshekov_sa <krivoshekov_sa@ursip.ru>
2023-08-28 21:13:04 +0500

    add use telegram hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-const tg = window.Telegram.WebApp
+import { useTelegram } from './hooks/useTelegram';
+import Button from './components/Button/Button';
 
 function App() {
+
+  const {onToggleButton, tg} = useTelegram
+
   useEffect(() => {
     tg.ready()
-  }, [])
+  }, [tg])
  
   return (
     <div className="App">
       <Header></Header>
+      <Button onClick={onToggleButton}>toggle</Button>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,14 @@
 import React from "react";
+import { useTelegram } from "../../hooks/useTelegram";
 import Button from "../Button/Button";
 
-const Header = (props) => {
-  const tg = window.Telegram.WebApp;
-
-  const onClose = () => {
-    tg.close();
-  };
+const Header = () => {
+  const { onClose, user } = useTelegram;
 
   return (
     <div className="header">
       <Button onClick={onClose}>Закрыть</Button>
-      <span className="username">{tg.initDataUnsafe?.user?.username}</span>
+      <span className="username">{user?.username}</span>
     </div>
   );
 };
