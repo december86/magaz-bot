5f1b26ca0d0892e3d62deee90496fd502bd4f254
krivoshekov_sa <krivoshekov_sa@ursip.ru>
2023-08-28 21:20:07 +0500

    fix hook using

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { useTelegram } from "../../hooks/useTelegram";
 import Button from "../Button/Button";
 
 const Header = () => {
-  const { onClose, user } = useTelegram;
+  const { onClose, user } = useTelegram();
 
   return (
     <div className="header">
